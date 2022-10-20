b27ff307ef1026d794d971c36e0b10ae5ba095b3
hidan1712 <116249104+hidan1712@users.noreply.github.com>
2022-10-20 12:23:49 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/hidandayne1712/';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Dành cho em';
+const contentCard = 'Đêm qua anh mơ thấy thiên thần, thiên thần hỏi anh muốn điều gì? Anh xin người hãy chăm sóc cho em. Nhưng người nói “Không!”. “Vì sao?” – anh hỏi. Người nói: “Thiên thần không thể chăm sóc cho một thiên thần khác”. Happy Vietnamese women’s day!';
+const giftImage = 'nhi.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
